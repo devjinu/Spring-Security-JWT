4bef33d2b240ddbd8705e4aa2b363b384e0fa663
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-02-24 15:31:47 +0900

    임시 토큰 테스트 완료

--- src/main/java/com/jwt/springsecurityjwt/filter/MyFilter3.java
+++ src/main/java/com/jwt/springsecurityjwt/filter/MyFilter3.java
@@ -1,12 +1,34 @@
 package com.jwt.springsecurityjwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터3");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+
+        // 토큰 : hello -> id, password 정상적으로 들어와서 로그인이 완료되면, 토큰을 만들어주고 그걸 응답을 해준다
+        // 요청할 때 마다 header에 Authorization에 value값으로 토큰을 가지고 넘어오면 이 토큰이 내가만든 토큰이 맞는지만 검증하면 됨(RSA, HS256)
+        if (req.getMethod().equals("POST")) {
+            System.out.println("post 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(" headerAuth : " + headerAuth);
+            System.out.println("필터3");
+
+            if (headerAuth.equals("hello")) {
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter writer = res.getWriter();
+                writer.println("인증 되지 않음");
+            }
+        }
+
     }
 }
